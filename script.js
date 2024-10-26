dd355c6a10aa748c5f6310c760687f25d123784f
Jay Singh Rathore <jayantsingh5173@gmail.com>
2024-10-26 11:57:38 +0530

    code

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// Get elements
-const circleButton = document.getElementById('circle-btn');
-const popup = document.getElementById('popup');
-const closeButton = document.getElementById('close-btn');
-
-// Open popup when the circle button is clicked
-circleButton.addEventListener('click', () => {
-    popup.style.display = 'flex';
-});
-
-// Close popup when the close button is clicked
-closeButton.addEventListener('click', () => {
-    popup.style.display = 'none';
-});
-
-// Close the popup if clicked outside the popup content
-popup.addEventListener('click', (event) => {
-    if (event.target === popup) {
-        popup.style.display = 'none';
-    }
-});
-
 const reviewCards = document.querySelector('.review-cards');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
